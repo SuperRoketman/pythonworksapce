050ca1afebadff74100904ad4c92da94ed5e1699
SuperRoketman <cook144k@gmail.com>
2021-09-15 16:09:20 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,7 +1,15 @@
-weather = input("how is the weather?")
-if weather == "rain":
-    print("bring a umbrella")
-elif weather == "dust":
-    print("bring a mask")
+# weather = input("how is the weather?")
+# if weather == "rain" or weather == "snow":
+#     print("bring a umbrella")
+# elif weather == "dust":
+#     print("bring a mask")
+# else:
+#     print("bring nothing")
+
+temp = int(input("how is the tempertuer?"))
+if 30 <= temp:
+    print("it is too hot. stay in")
+elif 10<= temp and temp<30:
+    print("bring a coat")
 else:
-    print("bring nothing")
+    print("it is too cold. stay in")
