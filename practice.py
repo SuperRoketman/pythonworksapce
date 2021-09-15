e0811e2e005bc95f7457e774399eeca32887b95e
SuperRoketman <cook144k@gmail.com>
2021-09-15 16:17:14 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,8 +1,3 @@
-# print("waiting : 1")
-# print("waiting : 2")
-# print("waiting : 3")
-# print("waiting : 4")
-
-
-for waiting_no in range(1, 101):
-    print("waitng : {0}".format(waiting_no))
+starbuks = ["ironman", "thor", "groot"]
+for customer in starbuks:
+    print("{0}, coffee is ready.".format(customer))
