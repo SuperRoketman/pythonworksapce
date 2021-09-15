b4afe4b02a9e6b958afd5a1651762d0103ad7c9e
SuperRoketman <cook144k@gmail.com>
2021-09-15 16:25:23 +0900

    Update practice.py

--- practice.py
+++ practice.py
@@ -1,3 +1,15 @@
-starbuks = ["ironman", "thor", "groot"]
-for customer in starbuks:
-    print("{0}, coffee is ready.".format(customer))
+#while
+# customer = "thor"
+# index = 5
+# while index >=1:
+#     print("{0},the coffee is ready. you have {1}more chance.".format(customer, index))
+#     index -= 1
+#     if index == 0:
+#         print("the coffee has been disposed of.")
+
+# customer = "ironman"
+# index =1
+# while True:
+#     print("{0}, the coffee is ready. i called you {1} times.".format(customer, index))
+#     index += 1
+
