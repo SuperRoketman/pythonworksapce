6ebc0d5541378d452abc3a5d4ed4690a3ffdd2ca
SuperRoketman <cook144k@gmail.com>
2021-09-20 22:36:07 +0900

    Python practice

--- practice.py
+++ practice.py
@@ -1,109 +1,120 @@
-# try:
-#     print("나누가 전용 계산기입니다.")
-#     nums = []
-#     nums.append(int(input("첫 번째 숫자를 입력하세요. : ")))
-#     nums.append(int(input("두 번째 숫자를 입력하세요. : ")))
-#     # nums.append(int(nums[0] / nums[1]))
-#     print("{0} / {1} = {2}".format(nums[0], nums[1], nums[2]))
-# except ValueError:
-#     print("에러! 잘못된 값을 입력하였습니다.")
-# except ZeroDivisionError as err:
-#     print(err)
-# except Exception as err:
-#     print("알 수 없는 에러가 발생하였습니다.")
-#     print(err)
+# import theater_module
+# theater_module.price(3) # 세 명이서 영화보러 갔을 때 가격
+# theater_module.price_morning(4) #네 명 조조할인가
+# theater_module.price_soldier(5) #다섯 명 군인할인가
 
-# class BigNumberError(Exception):
-#     def __init__(self, msg):
-#         self.msg = msg
+# import theater_module as mv
+# mv.price(3)
+# mv.price_morning(4)
+# mv.price_soldier(5)
 
-#     def __str__(self):
-#         return self.msg
+# from theater_module import*
+# # from random import*
+# price(3)
+# price_morning(4)
+# price_soldier(5)
 
-# try:
-#     print("한 자리 숫자 나누기 전용 계산기입니다.")
-#     num1 = int(input("첫 번째 숫자를 입력하세요. : "))
-#     num2 = int(input("두 번째 숫자를 입력하세요. : "))
-#     if num1 >= 10 or num2 >= 10:
-#         raise BigNumberError("입력값 : {0}, {1}".format(num1, num2))
-#     print("{0} / {1} = {2}".format(num1, num2, int(num1 / num2)))
-# except ValueError:
-#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
-# except BigNumberError as err:
-#     print("잘못된 값을 입력하였습니다. 한 자리 숫자만 입력하세요.")
-#     print(err)
-# finally:
-#     print("계산기를 이용해 주셔서 감사합니다.")
+# from theater_module import price, price_morning
+# price(5)
+# price_morning(6)
+# price_soldier(7)
+
+# from theater_module import price_soldier as price
+# price(5)
 
 
+# import travel.thailand
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
 
-# Quiz) 동네에 항상 대기 손님이 있는 맛있는 치킨집
-# 대기손님의 치킨 요리 시간을 줄이고자 자동 주문 시스템을 제작하였습니다.
-# 시스템 코드를 확인하고 적절한 예외처리 구문을 넣으시오.
+# import travel.thailand.ThailandPackage # import로 땡겨쓸때는 마지막이 모듈 혹은 패키지.
+# trip_to = travel.thailand.ThailandPackage()
+# trip_to.detail()
 
-# 조건1 : 1보다 작거나 숫자가 아닌 입력값이 들어올 때는 ValueError 로 처리
-#         출력 메시지 : "잘못된 값을 입력하였습니다."
-# 조건2 : 대기 손님이 주문할 수 있는 총 치킨량은 10마리로 한정
-#         치킨 소진 시 사용자 정의 에러[SoldOutError]를 발생시키고 프로그램 종료
-#         출력 메시지 : "재고가 소진되어 더이상 주문을 받지 않습니다."
+# from travel.thailand import ThailandPackage #from import 로 하면 클래스 혹은 함수까지 땡기기 가능
+# trip_to = ThailandPackage()
+# trip_to.detail()
 
-# [코드]
-
-#나 
-
-# class SoldOutError(Exception):
-#     def __init__(self):
-#         pass
-
-# try:
-#     chicken = 10
-#     waiting = 1 #홀은 현재 만석. 대기번호 1부터 시작
-#     while(True):
-#         print("[남은 치킨 : {0}]".format(chicken))
-#         order = int(input("치킨 몇 마리 주문하시겠습니까?"))
-#         if order <= 0:
-#             raise ValueError
-#         elif order > chicken:
-#             print("재료가 부족합니다.")
-#         else:
-#             print("[대기번호 {0}] {1}마리 주문이 완료되었습니다."\
-#                 .format(waiting, order))
-#             waiting += 1
-#             chicken -= order
-#             if chicken == 0:
-#                 raise SoldOutError
-# except ValueError:
-#     print("잘못된 값을 입력하였습니다.")
-# except SoldOutError:
-#     print("재고가 소진되어 더이상 주문을 받지 않습니다.")
-
-# 이렇게 하게되면 에러가 나면 프로그램이 종료가 됨. 밑에처럼 해야지 에러가 생겨도
-#while 문으로 돌아가게 됨.
+# from travel import vietnam
+# trip_to = vietnam.VietnamPackage()
+# trip_to.detail()
 
 
-class SoldOutError(Exception):
-    pass
+# from travel import thailand, vietnam
+# trip_to = thailand.ThailandPackage()
+# trip_to.detail()
 
-chicken = 10
-waiting = 1 #홀은 현재 만석. 대기번호 1부터 시작
-while(True):
-    try:
-        print("[남은 치킨 : {0}]".format(chicken))
-        order = int(input("치킨 몇 마리 주문하시겠습니까?"))
-        if order > chicken:
-            print("재료가 부족합니다.")
-        elif order <= 0:
-            raise ValueError
-        else:
-            print("[대기번호 {0}] {1}마리 주문이 완료되었습니다."\
-                .format(waiting, order))
-            waiting += 1
-            chicken -= order
 
-        if chicken == 0:
-            raise SoldOutError
-    except ValueError:
-        print("잘못된 값을 입력하였습니다.")
-    except SoldOutError:
-        print("재고가 소진되어 더이상 주문을 받지 않습니다.")
-        break
+# import inspect
+# import random
+# print(inspect.getfile(random))
+# print(inspect.getfile(thailand))
+
+# from bs4 import BeautifulSoup
+# soup = BeautifulSoup("<p>Some<b>bad<i>HTML")
+# print(soup.prettify())
+
+#내장함수
+# #input
+# language = input("무슨 언어를 좋아하세요?")
+# print("{0}은 아주 좋은 언어입니다!".format(language))
+
+# dir : 어던 객체를 넘겨줬을 때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
+# print(dir())
+# import random
+# print(dir())
+# import pickle
+# print(dir())
+
+# print(dir(random))
+
+# lst = [1, 2, 3]
+# print(dir(lst))
+
+# name = "jim"
+# print(dir(name))
+
+# glob : 경로 내의 폴더 / 파일 목록 조회 (윈도우 dir)
+# import glob
+# print(glob.glob("*.py"))
+
+# os : 운영체제에서ㅗ 제공하는 기본 기능
+# import os
+# print(os.getcwd())
+
+# folder = "sample_dir"
+
+# if os.path.exists(folder):
+#     print("이미 존재하는 폴더입니다.")
+#     os.rmdir(folder)
+#     print(folder, "폴더를 삭제하였습니다.")
+# else:
+#     os.makedirs(folder)
+#     print(folder, "폴더를 생성하였습니다.")
+# print(os.listdir())
+
+# import time
+# print(time.localtime())
+# print(time.strftime("%Y-%m-%d %H:%M:%S"))
+
+# import datetime
+# # print("오늘 날짜는 ", datetime.date.today())
+# today = datetime.date.today()
+# td = datetime.timedelta(days=100)
+# print("우리가 만난지 100일은", today + td)
+
+# Quiz) 프로젝트 내에 나만의 시그니처를 남기는 모듈을 만드시오
+
+# 조건 : 모듈 파일명은 byme.py 로 작성
+
+# (모듈 사용 예제)
+# import byme
+# byme.sige()
+
+# (출력 예제)
+# 이 프로그램은 최재윤에 의해 만들어졌습니다.
+# 유튜브 : http://youtube.com
+# 이메일 : cook144k@gmail.com
+
+import byme
+byme.sign()
