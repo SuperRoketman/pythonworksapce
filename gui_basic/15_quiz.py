cabe2d3402854395d1aa2e01fb51503ec82663b4
SuperRoketman <cook144k@gmail.com>
2021-10-03 21:23:51 +0900

    Python practice

--- gui_basic/15_quiz.py
+++ gui_basic/15_quiz.py
@@ -9,3 +9,52 @@
 # 4. 하단 status 바는 필요 없음
 # 6. 프로그램 크기, 위치는 자유롭게 하되 크기 조정이 가능해야 함
 # 7. 본문 우측에 상하 스크롤 바 넣기
+
+import os
+from tkinter import*
+
+root = Tk()
+root.title("제목 없음 - Windows 메모장")
+root.geometry("640x480")
+
+# 열기, 저장 파일 이름
+filename = "mynote.txt"
+
+def open_file():
+    if os.path.isfile(filename): # 파일 있으면 True, 없으면 False
+        with open(filename, "r", encoding="utf8") as file:
+            txt.delete("1.0", END)
+            txt.insert(END, file.read())
+
+
+
+
+def save_file():
+    with open(filename, "w", encoding="utf8") as file:
+        file.write(txt.get("1.0", END))
+
+menu = Menu(root)
+
+# 메뉴
+menu_file = Menu(root, tearoff=0)
+menu_file.add_command(label="열기", command=open_file)
+menu_file.add_command(label="저장", command=save_file)
+menu_file.add_command(label="끝내기", command=root.quit)
+menu.add_cascade(label="파일", menu=menu_file)
+
+menu.add_cascade(label="편집")
+menu.add_cascade(label="서식")
+menu.add_cascade(label="보기")
+menu.add_cascade(label="도움말")
+
+# 스크롤 바
+scrollbar = Scrollbar(root)
+scrollbar.pack(side="right", fill="both")
+
+# 글쓰기
+txt = Text(root, yscrollcommand = scrollbar.set)
+txt.pack(fill="both", expand=True)
+
+root.config(menu=menu)
+scrollbar.config(command=txt.yview)
+root.mainloop()
