4a0144d434acf6066543f5a7f245a8f1830bfe0e
SuperRoketman <cook144k@gmail.com>
2021-10-16 17:23:41 +0900

    webscraping practice

--- webscraping_basic/16_selenium_movies_scroll.py
+++ webscraping_basic/16_selenium_movies_scroll.py
@@ -1,26 +1,3 @@
-# import requests
-# from bs4 import BeautifulSoup
-
-# url = "https://play.google.com/store/movies/top"
-# headers = {
-#     "User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.81 Safari/537.36",
-#     "Accept-Language":"ko-KR,ko"
-#     }
-# res = requests.get(url, headers=headers)
-# res.raise_for_status()
-# soup = BeautifulSoup(res.text, "lxml")
-
-# movies = soup.find_all("div", attrs={"class":"ImZGtf mpg5gc"})
-# print(len(movies))
-
-# # with open("movie.html", "w", encoding="utf8") as f:
-# #     # f.write(res.text)
-# #     f.write(soup.prettify())
-
-# for movie in movies:
-#     title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
-#     print(title)
-
 from selenium import webdriver
 browser = webdriver.Chrome()
 browser.maximize_window()
@@ -59,3 +36,44 @@ while True:
 
     prev_height = curr_height
 print("스크롤 완료")
+
+
+
+import requests
+from bs4 import BeautifulSoup
+
+soup = BeautifulSoup(browser.page_source, "lxml")
+
+# movies = soup.find_all("div", attrs={"class":["ImZGtf mpg5gc","Vpfmgd"]})
+movies = soup.find_all("div", attrs={"class":"Vpfmgd"})
+print(len(movies))
+
+# with open("movie.html", "w", encoding="utf8") as f:
+#     # f.write(res.text)
+#     f.write(soup.prettify())
+
+for movie in movies:
+    title = movie.find("div", attrs={"class":"WsMG1c nnK0zc"}).get_text()
+    # print(title)
+
+    # 할인 전 가격
+    original_price = movie.find("span", attrs={"class":"SUZt4c djCuy"})
+    if original_price:
+        original_price = original_price.get_text()
+    else:
+        # print(title, "<할인되지 않은 영화 제외>")
+        continue
+
+    # 할인된 가격
+    price = movie.find("span", attrs={"class":"VfPpfd ZdBevf i5DZme"}).get_text()
+
+    link = movie.find("a", attrs={"class":"JC71ub"})["href"]
+    # https://play.google.com + /store/movies/details/%EB%A7%88%ED%81%AC%EB%A7%A8?id=4IfZtOlXxuA.P
+
+    print(f"제목 : {title}")
+    print(f"할인 전 가격 : {original_price}")
+    print(f"할인 후 금액 : {price}")
+    print("링크 : ", "https://play.google.com" + link)
+    print("-"*120)
+
+browser.quit()
