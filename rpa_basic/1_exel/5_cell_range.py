20b0d9669449bfc7b6ca7029ff0fbb02fef233cc
SuperRoketman <cook144k@gmail.com>
2021-10-19 23:48:20 +0900

    rpa practice

--- rpa_basic/1_exel/5_cell_range.py
+++ rpa_basic/1_exel/5_cell_range.py
@@ -13,18 +13,54 @@ for i in range(1, 11): # 10개 데이터 넣기
 # for cell in col_B:
 #     print(cell.value)
 
-col_range = ws["B:C"] # 영어, 수학 column 함께 가지고 오기
+# col_range = ws["B:C"] # 영어, 수학 column 함께 가지고 오기
 # for cols in col_range:
 #     for cell in cols:
 #         print(cell.value)
 
-row_title = ws[1] # 1번째 row 만 가지고 오기
+# row_title = ws[1] # 1번째 row 만 가지고 오기
 # for cell in row_title:
 #     print(cell.value)
 
-row_range = ws[2:6] # 2번째 줄에서 6번째 줄까지 가지고 오기
-for rows in row_range:
-    for cell in rows:
-        print(cell.value, end=" ")
+# row_range = ws[2:6] # 2번째 줄에서 6번째 줄까지 가지고 오기
+# for rows in row_range:
+#     for cell in rows:
+#         print(cell.value, end=" ")
+#     print()
+
+# from openpyxl.utils.cell import coordinate_from_string
+
+# row_range = ws[2:ws.max_row] # 2번째 줄부터 마지막 줄까지
+# for rows in row_range:
+#     for cell in rows:
+#         # print(cell.value, end=" ")
+#         # print(cell.coordinate, end=" ") # A10, AZ250 일 땐 슬라이싱이 힘들다
+#         xy = coordinate_from_string(cell.coordinate)
+#         # print(xy, end=" ")
+#         print(xy[0], end="")
+#         print(xy[1], end=" ")
+#     print()
+
+# 전체 rows
+# print(tuple(ws.rows))
+# for row in tuple(ws.rows):
+#     print(row[0].value)
+
+# 전체 columns
+# print(tuple(ws.columns))
+# for column in tuple(ws.columns):
+#     print(column[1].value)
+
+# for row in ws.iter_rows(): # 전체 row
+#     print(row[2].value)
+
+# for column in ws.iter_cols():
+#     print(column[1].value)
+
+# for row in ws.iter_rows(min_row=2, max_row=11, min_col=2, max_col=3): # 전체 row
+#     print(row) # 수학, 영어
+
+for col in ws.iter_cols(min_row=1, max_row=5, min_col=1, max_col=3):
+    print(col)
 
 wb.save("sample.xlsx")
